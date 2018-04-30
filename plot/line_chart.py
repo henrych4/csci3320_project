279bce2dce85dd8f16e28cfd977f23e5283ab683
hhhung <singyinhenry@gmail.com>
2018-04-30 18:57:58 +0800

    start from nearest date and set ylim

--- plot/line_chart.py
+++ plot/line_chart.py
@@ -13,9 +13,10 @@ if df_train.loc[df_train['horse_id']==str(horseID)].empty :
     print("invalid input!")
 else:
     df_horse = df_train.loc[df_train['horse_id']==horseID]
-    df_horse = df_horse[::-1][:6]
+    df_horse = df_horse[-6:]
     plt.title("Recent Racing Result of "+ str(horseID))
     plt.xlabel('race_id')
     plt.ylabel('finishing_position')
+    plt.ylim(0, 15)
     plt.plot(df_horse['race_id'],df_horse['finishing_position'])
     plt.show()
